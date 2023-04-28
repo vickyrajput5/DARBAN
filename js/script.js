b983570c10fc75431e03f5d28cbe650aa0ddd3b0
vickyrajput5 <waqarmuhammad337@gmail.com>
2023-04-28 07:34:00 -0700

    newproject

---
+++ js/script.js
@@ -0,0 +1,31 @@
+var typed = new Typed('.typed',{
+    strings: ['Food ', 'Food '],
+    typeSpeed: 55,
+    startDelay: 500,
+    backDelay: 1000,
+    backSpeed: 55,
+    fadeOut: true,
+    loop: true,
+    showCursor: false,
+    
+})
+var typed = new Typed('.typed2',{
+    strings: ['Drink', 'Drink'],
+    typeSpeed: 55,
+    startDelay: 500,
+    backDelay: 1000,
+    backSpeed: 55,
+    fadeOut: true,
+    loop: true,
+    showCursor: false,
+})
+var typed = new Typed('.typed3',{
+    strings: ['BRING YOUR BAO’S HOME!', 'BRING YOUR BAO’S HOME!'],
+    typeSpeed: 75,
+    startDelay: 1500,
+    backDelay: 1000,
+    backSpeed: 75,
+    fadeOut: true,
+    loop: true,
+    showCursor: false,
+})
